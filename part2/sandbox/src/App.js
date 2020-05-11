c27fac813c812b2400e99d6e0bcd057b7b450504
takecare <rui@vazteixeira.org>
2020-05-11 13:49:30 +0100

    part 2c

--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [input, setInput] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    axios
+      .get("http://localhost:3001/notes")
+      .then((response) => setNotes(response.data));
+  };
+  useEffect(hook, []);
 
   const addNote = (event) => {
     event.preventDefault();
