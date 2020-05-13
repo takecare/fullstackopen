4b04a3d8bfcce289dfc2472c22f537299e43e08a
takecare <rui@vazteixeira.org>
2020-05-13 18:47:10 +0100

    remove cors support as backend and frontend will run from same server

--- part3/exercises/phonebook/backend/index.js
+++ part3/exercises/phonebook/backend/index.js
@@ -5,13 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  next();
-});
-
 morgan.token("body", (req) => util.inspect(req.body));
 app.use(
   morgan((tokens, req, res) => {
