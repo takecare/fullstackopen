df2832993d918581b6349b046cdd8730619fd009
takecare <rui@vazteixeira.org>
2020-05-13 15:47:57 +0100

    allow all cors on sandbox

--- part3/sandbox/backend/index.js
+++ part3/sandbox/backend/index.js
@@ -14,7 +14,7 @@ app.use(logger);
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // http://localhost:3000
   next();
 });
 
