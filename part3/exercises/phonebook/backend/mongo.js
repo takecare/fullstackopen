a9aa672827a3edfe10fd86ad9f0ae422fa077074
takecare <rui@vazteixeira.org>
2020-05-14 10:36:25 +0100

    exercise 3.12

---
+++ part3/exercises/phonebook/backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const usage = "Usage: node mongo.js <password> [name] [number]";
+const args = process.argv.slice(2);
+
+if (args.length == 0) {
+  console.log(usage);
+  process.exit(1);
+}
+
+const password = args[0];
+const url = `mongodb+srv://fullstack:${password}@fullstack-u4b60.mongodb.net/numbers-app?retryWrites=true&w=majority`;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const numberSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+const Number = mongoose.model("Number", numberSchema);
+
+if (args.length == 1) {
+  Number.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((number) => console.log(number));
+    mongoose.connection.close();
+  });
+} else if (args.length == 3) {
+  const number = new Number({
+    name: args[1],
+    number: args[2],
+  });
+  number.save().then((result) => {
+    console.log("added");
+    mongoose.connection.close();
+  });
+} else {
+  console.log(usage);
+  process.exit(1);
+}
---
+++ part3/sandbox/backend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@fullstack-u4b60.mongodb.net/notes-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is Easy",
+  date: new Date(),
+  important: true,
+});
+
+note.save().then((response) => {
+  console.log("note saved!");
+  Note.find({}).then((result) => {
+    result.forEach((note) => {
+      console.log(note);
+    });
+    mongoose.connection.close();
+  });
+});
