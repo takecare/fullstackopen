4c7ef421cdb34a896e0fd41ea6e4da7b0e3b0c16
takecare <rui@vazteixeira.org>
2020-05-19 10:42:09 +0100

    add support for environments

--- part4/exercises/blog/backend/utils/logger.js
+++ part4/exercises/blog/backend/utils/logger.js
@@ -1,8 +1,7 @@
-const info = console.log;
+const info = (...params) =>
+  process.env.NODE_ENV === "test" ? () => {} : console.log(...params);
 
-const error = console.error;
+const error = (...params) =>
+  process.env.NODE_ENV === "test" ? () => {} : console.error(...params);
 
-module.exports = {
-  info,
-  error,
-};
+module.exports = { info, error };
