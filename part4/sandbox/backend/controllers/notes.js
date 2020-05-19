8556dc1054fcbb2ee819a9dab044f9ae6345c459
takecare <rui@vazteixeira.org>
2020-05-19 10:32:56 +0100

    refactor to async/await

--- part4/sandbox/backend/controllers/notes.js
+++ part4/sandbox/backend/controllers/notes.js
@@ -10,52 +10,53 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        return res.json(note.toJSON());
-      } else {
-        throw { error: "NotFound" };
-      }
-    })
-    .catch((error) => next(error));
+router.get("/:id", async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    res.json(note.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/:id", (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(() => res.status(204).end())
-    .catch((error) => next(error));
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Note.findByIdAndRemove(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const newNote = new Note({
     important: false,
     ...req.body,
     date: new Date(),
   });
 
-  newNote
-    .save()
-    .then(() => newNote.toJSON())
-    .then((note) => res.status(201).send(note))
-    .catch((error) => next(error));
+  try {
+    const note = await newNote.save();
+    res.status(201).send(note.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   const updateNote = {
     content: req.body.content,
     important: req.body.important,
   };
-  Note.findByIdAndUpdate(req.params.id, updateNote, { new: true })
-    .then((result) => {
-      if (result) {
-        return res.json(result.toJSON());
-      } else {
-        throw { name: "NotFound" };
-      }
-    })
-    .catch((error) => next(error));
+
+  try {
+    const result = await Note.findByIdAndUpdate(req.params.id, updateNote, {
+      new: true,
+    });
+    res.json(result.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
