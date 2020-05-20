97213be070dcddd3de4011d29ca090db2afbdeb0
takecare <rui@vazteixeira.org>
2020-05-20 16:54:26 +0100

    add user route

--- part4/sandbox/backend/app.js
+++ part4/sandbox/backend/app.js
@@ -5,6 +5,7 @@ const errorHandler = require("./middleware/errorhandler");
 const unsupportedEndpoint = require("./middleware/unsupportedendpoint");
 const mongoose = require("./utils/mongoose");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 
 mongoose.connect();
 
@@ -16,6 +17,7 @@ app.use(logger);
 app.use(cors);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(unsupportedEndpoint);
 app.use(errorHandler);
