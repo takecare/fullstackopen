c539dd2316d18f8fc045c3d42790a1688ba57bf7
takecare <rui@vazteixeira.org>
2020-05-20 16:54:11 +0100

    add user ref to note model

--- part4/sandbox/backend/models/note.js
+++ part4/sandbox/backend/models/note.js
@@ -4,11 +4,18 @@ const noteSchema = new mongoose.Schema({
   content: { type: String, minlength: 3, required: true },
   date: { type: Date, required: true },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
+    if (returnedObject.userId) {
+      returnedObject.userId = returnedObject.user._id.toString();
+    }
     delete returnedObject._id;
     delete returnedObject.__v;
   },
