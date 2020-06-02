cdf777a7cf36145772f00ffce537ae9ef9a15b48
takecare <rui@vazteixeira.org>
2020-06-02 16:55:01 +0100

    add eslint cfg on backend

---
+++ part5/exercises/backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: ["jest"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
