1fa62c1dcf9e24a6854c56bb28a40cfc6f164e1f
takecare <rui@vazteixeira.org>
2020-06-03 10:33:35 +0100

    shutdown server gracefully

--- part5/exercises/backend/index.js
+++ part5/exercises/backend/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
@@ -7,3 +8,13 @@ const server = http.createServer(app);
 server.listen(config.port, () => {
   logger.info(`Server listening on port ${config.port}`);
 });
+
+const shutdown = () => {
+  server.close(() => {
+    mongoose.connection.close(false, () => process.exit(0));
+  });
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGQUIT", shutdown);
+process.on("SIGTERM", shutdown);
