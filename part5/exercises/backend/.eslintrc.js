1e3ff3d5985160d39a5f061bec857a311e298796
takecare <rui@vazteixeira.org>
2020-06-08 15:34:41 +0100

    fix quotes formatting in exercises

--- part5/exercises/backend/.eslintrc.js
+++ part5/exercises/backend/.eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
+    quotes: ["error", "double", { avoidEscape: true }],
     semi: ["error", "always"],
   },
 };
