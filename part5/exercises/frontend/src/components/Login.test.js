4fec3ed956845e1f886c53d4c1c9e13fd4dab0a8
takecare <rui@vazteixeira.org>
2020-06-15 11:22:39 +0100

    add snapshot test for login component

--- part5/exercises/frontend/src/components/Login.test.js
+++ part5/exercises/frontend/src/components/Login.test.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import Login from './Login';
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<Login onLogin={() => {}} />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
