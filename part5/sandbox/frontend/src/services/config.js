c6b7d9831571be40f60d267c1c9ff818d2557ea0
takecare <rui@vazteixeira.org>
2020-08-27 08:54:58 +0100

    load api urls from different envs

--- part5/sandbox/frontend/src/services/config.js
+++ part5/sandbox/frontend/src/services/config.js
@@ -1,6 +1,2 @@
-const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? process.env.REACT_APP_API_BASE_URL_DEV
-    : process.env.REACT_APP_API_BASE_URL_PROD; // TODO add test env
-
+let baseUrl = process.env.REACT_APP_API_BASE_URL;
 export { baseUrl };
