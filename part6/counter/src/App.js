8810e7982bcbf18a97f68e1e4df25e786deee7e6
takecare <rui@vazteixeira.org>
2020-09-14 09:06:56 +0100

    p6s convert counter to redux

--- part6/counter/src/App.js
+++ part6/counter/src/App.js
@@ -1,28 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    case "RESET":
-      return 0;
-    default:
-      return state;
-  }
-};
-
-function App() {
-  const [counter, setCounter] = useState(0);
-
+function App({ store }) {
   return (
     <div className="App">
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <button onClick={() => setCounter(0)}>reset</button>
+      <div>{store.getState()}</div>
+      <button onClick={(event) => store.dispatch({ type: "INCREMENT" })}>
+        +
+      </button>
+      <button onClick={(event) => store.dispatch({ type: "DECREMENT" })}>
+        -
+      </button>
+      <button onClick={(event) => store.dispatch({ type: "RESET" })}>
+        reset
+      </button>
     </div>
   );
 }
--- part6/counter/src/index.js
+++ part6/counter/src/index.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "RESET":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+render();
+store.subscribe(render);
